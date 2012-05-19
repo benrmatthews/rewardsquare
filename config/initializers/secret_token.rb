4e49a0c5ad41bab79ffeca7320025077a9238b0d
Ben Matthews <ben@brightone.org.uk>
2012-05-19 12:31:17 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rewardsquare::Application.config.secret_token = '7568926fe8ff00ed651abd8ae9cd85e643fb65febf78bad84561a86ca785d6ec17eb89d0ba011901adbc7bf996ed6baea306c55a6010626beb50d7d230640e3d'
